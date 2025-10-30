3925c574997d95395012b4990b9dfbad05a958b6
moranjohn-95 <moranjohn95@gmail.com>
2025-10-30 18:56:45 +0000

    Create constants file and add allowed categories for transactions

---
+++ python_scripts/utilities/constants.py
@@ -0,0 +1,19 @@
+"""
+constants.py
+-------------
+Shared constants and allowed value lists.
+- Added to make reports more accurate.
+"""
+
+ALLOWED_CATEGORIES = [
+    "groceries",
+    "house-bills",
+    "transport",
+    "social",
+    "health",
+    "work-related",
+    "subscriptions",
+    "entertainment",
+    "savings",
+    "misc",
+]
